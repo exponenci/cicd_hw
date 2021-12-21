666a0796256c34684eb217184ebfd888ab5d74e9
Saidakbar Gaziev <si.gaziev@gmail.com>
2021-12-21 13:13:44 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.utils.exceptions import BotBlocked
+from os import getenv
+from sys import exit
+
+bot_token = getenv("BOT_TOKEN")
+if not bot_token:
+    exit("Error: no token provided")
+
+bot = Bot(token=bot_token)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
+
+
+@dp.errors_handler(exception=BotBlocked)
+async def error_bot_blocked(update: types.Update, exception: BotBlocked):
+    print(f"Bot has been blocked\nMessage: {update}\nError: {exception}")
+    return True
+
+
+@dp.message_handler()
+async def cmd_test1(message: types.Message):
+    await message.reply(message.text)
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
